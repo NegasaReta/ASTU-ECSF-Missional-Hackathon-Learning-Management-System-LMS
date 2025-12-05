39706b4cb757c78241a9de5be51558d89569b414
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-12-05 23:16:48 +0300

    creating a configuration file for env files

---
+++ backend/app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DBSettings(BaseSettings):
+    NEON_DB: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file="./.env",
+        env_ignore_empty=True,
+        extra="ignore",
+        validate_default=False,
+    )
+
+    @property
+    def online_url(self):
+        return self.NEON_DB
+
+
+settings = DBSettings()
