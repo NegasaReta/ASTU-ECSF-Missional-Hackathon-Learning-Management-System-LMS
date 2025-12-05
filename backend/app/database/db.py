d67586ba077bd67be6134ae4b6da39f958cceacb
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-12-05 23:20:55 +0300

    creating a database file

---
+++ backend/app/database/db.py
@@ -0,0 +1,21 @@
+from sqlmodel import SQLModel, create_engine, Session
+from app.core.config import settings
+
+# Engine
+engine = create_engine(
+    settings.online_url,
+    echo=True,
+)
+
+
+# Create tables
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    session = Session(engine)
+    try:
+        yield session
+    finally:
+        session.close()
