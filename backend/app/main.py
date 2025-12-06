c4f26bca9a94e73b61a451a8abdba4ec4e1235aa
NegasaReta <negareta83@gmail.com>
2025-12-06 12:27:49 +0300

    Routing Modifies

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,11 +23,12 @@ app.add_middleware(
 )
 
 # Routers
+app.include_router(team_generation.router)
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(missionary.router)
 app.include_router(recipient.router)
-app.include_router(team_generation.router)
+
 
 @app.on_event("startup")
 def on_startup():
