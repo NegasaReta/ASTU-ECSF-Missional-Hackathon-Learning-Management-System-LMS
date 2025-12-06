de8503de1200589b2cbf11d84a6781c9acbc829f
NegasaReta <negareta83@gmail.com>
2025-12-07 02:41:27 +0300

    added register and login route

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,6 +41,29 @@ def on_startup():
 def register_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/register", response_class=HTMLResponse)
+def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/dashboard", response_class=HTMLResponse)
+def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+@app.get("/communication", response_class=HTMLResponse)
+def communication(request: Request):
+    return templates.TemplateResponse("communication.html", {"request": request})
+
+@app.get("/attendance", response_class=HTMLResponse)
+def attendance(request: Request):
+    return templates.TemplateResponse("attendance.html", {"request": request})
+
+@app.get("/pairing", response_class=HTMLResponse)
+def pairing(request: Request):
+    return templates.TemplateResponse("pairing.html", {"request": request})
 @app.get("/")
 def main(user=Depends(get_current_user)):
     return {"message": "home page"}
